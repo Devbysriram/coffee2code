44a26c9a61f756b5adc2aea926da4c88acee7653
Sriram R <sriramrk9495@gmail.com>
2024-10-19 08:15:18 +0530

    logochange

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import './navbar.css'
 import htmlLogo from '../../assets/html.png'
 import jsLogo from '../../assets/js.svg'
-import codeLogo from '../../assets/code.png'
+import codeLogo from '../../assets/logo.webp'
 
 const Navbar = () => {
   return ( 
     <div className='navbar'>
       <div className="app-title">
-        <img src={codeLogo} width={32} style={{marginRight: 10}} alt="" />
-        <h3>Coffee2Code</h3>
+        <img src={codeLogo} width={200} style={{marginRight: 10}} alt="" />
+        
       </div>
         <a href="/" className='nav-items'>
           <img src={htmlLogo} width={32} style={{marginRight: 10}} alt="" />
